53b913f9484b01968e750532cdda600ac07d9e1b
Javier Robledo <javier.robledo@usm.cl>
2018-07-22 12:01:03 -0400

    lda

--- clustering.py
+++ clustering.py
@@ -242,17 +242,6 @@ def lda_directory_exists(dataset_name):
 def create_lda_directory(dataset_name):
     os.makedirs(get_directory_lda_dataset(dataset_name))
 
-#TODO in lda the clasification is documents and terms?
-def lda_exists(dataset_name,preprocessing,mindf,k,ngram_min,ngram_max):
-    directory = get_directory_lda_dataset(dataset_name)
-    filenamedoc = "{ds}_{pr}_{df}_{k}_{mi}_{ma}_lda_documents".format(
-        ds=dataset_name, pr=preprocessing, df=mindf, k=k, mi=ngram_min, ma=ngram_max)
-    fullpathdoc = os.path.join(directory, filenamedoc)+".npy"
-    filenameterm = "{ds}_{pr}_{df}_{k}_{mi}_{ma}_lda_terms".format(
-        ds=dataset_name, pr=preprocessing, df=mindf, k=k, mi=ngram_min, ma=ngram_max)+".npy"
-    fullpathterm = os.path.join(directory, filenameterm)
-    return os.path.exists(fullpathdoc) and os.path.exists(fullpathterm)
-
 def corpus_vektorizer_exists(dataset_name,preprocessing):
     directory = get_directory_lda_dataset(dataset_name)
     filenametfidf = "{ds}_{pr}_count_vektorizer".format(ds=dataset_name, pr=preprocessing) + ".h5"
@@ -303,6 +292,84 @@ def load_vekt_data(dataset_name,preprocessing):
 def from_lda_to_clusters():
     return
 
+def save_data_lda(dataset_name,preprocessing,mindf,k,ngram_min,ngram_max,model,terms):
+    directory = get_directory_lda_dataset(dataset_name)
+    #Matrix of topics. First vektor is topic 0, second is topic 1...
+    #The numbers inside the vectors are frequencies
+    filenamemodel = "{ds}_{pr}_{df}_{k}_{mi}_{ma}_lda_model".format(
+        ds=dataset_name, pr=preprocessing, df=mindf, k=k, mi=ngram_min, ma=ngram_max)
+    fullpathmodel = os.path.join(directory, filenamemodel)
+    filenameterm = "{ds}_{pr}_{df}_{k}_{mi}_{ma}_lda_terms".format(
+        ds=dataset_name, pr=preprocessing, df=mindf, k=k, mi=ngram_min, ma=ngram_max)
+    fullpathterm = os.path.join(directory, filenameterm)
+    save(fullpathmodel, model.astype(float32))
+    save(fullpathterm, terms)
+
+def lda_exists(dataset_name,preprocessing,mindf,k,ngram_min,ngram_max):
+    directory = get_directory_lda_dataset(dataset_name)
+    filenamedoc = "{ds}_{pr}_{df}_{k}_{mi}_{ma}_lda_model".format(
+        ds=dataset_name, pr=preprocessing, df=mindf, k=k, mi=ngram_min, ma=ngram_max)
+    fullpathdoc = os.path.join(directory, filenamedoc)+".npy"
+    filenameterm = "{ds}_{pr}_{df}_{k}_{mi}_{ma}_lda_terms".format(
+        ds=dataset_name, pr=preprocessing, df=mindf, k=k, mi=ngram_min, ma=ngram_max)+".npy"
+    fullpathterm = os.path.join(directory, filenameterm)
+    return os.path.exists(fullpathdoc) and os.path.exists(fullpathterm)
+
+def get_directory_lda_dataset_periods(dataset_name,preprocessing,start,end,n):
+    directory = get_directory_lda_dataset(dataset_name)
+    newdirectory = "{p}_{s}-{e}_{n}".format(p=preprocessing,s=start,e=end,n=n)
+    return os.path.join(directory,newdirectory)
+
+def corpus_vektorizer_periods_exists(dataset_name,preprocessing,start,end,n):
+    directory = get_directory_lda_dataset_periods(dataset_name,preprocessing,start,end,n)
+    return os.path.exists(directory)
+
+def store_vekt_data_periods(dataset_name,preprocessing,start,end,n,i,j,tfidf,documents,terms):
+    directory = get_directory_lda_dataset_periods(dataset_name,preprocessing,start,end,n)
+    filenametfidf = "{i}-{j}_count_vektorizer".format(i=i,j=j) + ".h5"
+    filenamedocuments = "{i}-{j}_documents_vektorizer".format(i=i,j=j)
+    filenameterms = "{i}-{j}_terms_vektorizer".format(i=i,j=j)
+    fullpathtfidf = os.path.join(directory, filenametfidf)
+    fullpathdocuments = os.path.join(directory, filenamedocuments)
+    fullpathterms = os.path.join(directory, filenameterms)
+    store_sparse_mat(tfidf, "count", fullpathtfidf)
+    save(fullpathdocuments,documents)
+    save(fullpathterms, terms)
+
+def load_vekt_data_periods(dataset_name,preprocessing,start,end,n,i,j):
+    directory = get_directory_lda_dataset_periods(dataset_name,preprocessing,start,end,n)
+    filenametfidf = "{i}-{j}_count_vektorizer".format(i=i,j=j) + ".h5"
+    filenamedocuments = "{i}-{j}_documents_vektorizer".format(i=i,j=j)
+    filenameterms = "{i}-{j}_terms_vektorizer".format(i=i,j=j)
+    fullpathtfidf = os.path.join(directory, filenametfidf)
+    fullpathdocuments = os.path.join(directory, filenamedocuments)
+    fullpathterms = os.path.join(directory, filenameterms)
+    tfidf = load_sparse_mat("count",fullpathtfidf).astype(float32)
+    documents = load(fullpathdocuments+".npy")
+    terms = load(fullpathterms + ".npy")
+    return tfidf,documents,terms
+
+def lda_periods_exists(dataset_name,preprocessing,mindf,k,ngram_min,ngram_max,start,end,n,i,j):
+    directory = get_directory_lda_dataset_periods(dataset_name, preprocessing, start, end, n)
+    filenamedoc = "{i}-{j}_{df}_{k}_{mi}_{ma}_lda_model".format(i=i,j=j,
+        ds=dataset_name, pr=preprocessing, df=mindf, k=k, mi=ngram_min, ma=ngram_max)
+    fullpathdoc = os.path.join(directory, filenamedoc)+".npy"
+    filenameterm = "{i}-{j}_{df}_{k}_{mi}_{ma}_lda_terms".format(i=i,j=j,
+        ds=dataset_name, pr=preprocessing, df=mindf, k=k, mi=ngram_min, ma=ngram_max)+".npy"
+    fullpathterm = os.path.join(directory, filenameterm)
+    return os.path.exists(fullpathdoc) and os.path.exists(fullpathterm)
+
+def save_data_lda_periods(dataset_name,preprocessing,mindf,k,ngram_min,ngram_max,model,terms,start,end,n,i,j):
+    directory = get_directory_lda_dataset_periods(dataset_name,preprocessing,start,end,n)
+    filenamedoc = "{i}-{j}_{df}_{k}_{mi}_{ma}_lda_model".format(i=i,j=j,
+        df=mindf,k=k,mi=ngram_min,ma=ngram_max)
+    fullpathdoc = os.path.join(directory, filenamedoc)
+    filenameterm = "{i}-{j}_{df}_{k}_{mi}_{ma}_lda_terms".format(i=i,j=j,
+        df=mindf, k=k, mi=ngram_min, ma=ngram_max)
+    fullpathterm = os.path.join(directory, filenameterm)
+    save(fullpathdoc, model.astype(float32))
+    save(fullpathterm, terms)
+
 def lda(dataset_name,full,preprocessing,mindf,k,ngram_min,ngram_max,start,end,n):
     if not lda_directory_exists(dataset_name):
         create_lda_directory(dataset_name)
@@ -316,33 +383,23 @@ def lda(dataset_name,full,preprocessing,mindf,k,ngram_min,ngram_max,start,end,n)
             words = v.get_feature_names()
             store_vekt_data(dataset_name, preprocessing, X, docnames, words)
         countvekt, documents, terms = load_vekt_data(dataset_name, preprocessing)
-        lda = LatentDirichletAllocation(n_topics=k, max_iter=5, learning_method='online',
+        if not lda_exists(dataset_name,preprocessing,mindf,k,ngram_min,ngram_max):
+            lda = LatentDirichletAllocation(n_topics=k, max_iter=5, learning_method='online',
                                         learning_offset=50., random_state=0).fit(countvekt)
-        return lda
-
-    #    if not lda_exists(dataset_name,preprocessing,mindf,k,ngram_min,ngram_max):
-    #         start = time.time()
-    #         lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=10, id2word=dictionary)
-    #         lda_model.fit(tfidf)
-    #         end = time.time()
-    #         print("LDA process takes", int(round(end - start)), "seconds")
-    #         save_clasification(get_directory_lda_dataset(dataset_name),dataset_name,preprocessing,mindf,k1,k2,ngram_min,ngram_max,model)
-    # else:
-    #     time_corpus = split_data_in_time_slices(corpus, start, end, n)
-    #     if not tfidf_periods_exists(dataset_name,preprocessing,start,end,n):
-    #         os.makedirs(get_directory_dataset_periods(dataset_name,preprocessing,start,end,n))
-    #         for (s, e), corp in time_corpus.items():
-    #             texts = corp.text.values
-    #             docnames = corp.text.index.values
-    #             X, v = create_tfidf(texts, mindf, ngram_min, ngram_max)
-    #             words = v.get_feature_names()
-    #             store_data_periods(dataset_name,preprocessing,start,end,n,s,e,X,docnames,words)
-    #     for s,e in time_corpus:
-    #         tfidf, documents, terms = load_data_periods(dataset_name, preprocessing,start,end,n,s,e)
-    #         if not spectral_periods_exists(dataset_name,preprocessing,mindf,k1,k2,ngram_min,ngram_max,start,end,n,s,e):
-    #             st = time.time()
-    #             model = SpectralBiclustering(n_clusters=(k1, k2), random_state=0)
-    #             model.fit(tfidf)
-    #             ed = time.time()
-    #             print("Biclustering process takes", int(round(ed - st)), "seconds")
-    #             save_clasification_periods(dataset_name,preprocessing,mindf,k1,k2,ngram_min,ngram_max,model,start,end,n,s,e)
+            save_data_lda(dataset_name,preprocessing,mindf,k,ngram_min,ngram_max,lda.components_,terms)
+    else:
+        time_corpus = split_data_in_time_slices(corpus, start, end, n)
+        if not corpus_vektorizer_periods_exists(dataset_name,preprocessing,start,end,n):
+            os.makedirs(get_directory_lda_dataset_periods(dataset_name,preprocessing,start,end,n))
+            for (s, e), corp in time_corpus.items():
+                texts = corp.text.values
+                docnames = corp.text.index.values
+                X, v = create_corpus_vektorizer(texts, mindf, ngram_min, ngram_max)
+                words = v.get_feature_names()
+                store_vekt_data_periods(dataset_name,preprocessing,start,end,n,s,e,X,docnames,words)
+        for s,e in time_corpus:
+            countvekt, documents, terms = load_vekt_data_periods(dataset_name, preprocessing,start,end,n,s,e)
+            if not lda_periods_exists(dataset_name,preprocessing,mindf,k,ngram_min,ngram_max,start,end,n,s,e):
+                lda = LatentDirichletAllocation(n_topics=k, max_iter=5, learning_method='online',
+                                                learning_offset=50., random_state=0).fit(countvekt)
+                save_data_lda_periods(dataset_name,preprocessing,mindf,k,ngram_min,ngram_max,lda.components_,terms,start,end,n,s,e)
